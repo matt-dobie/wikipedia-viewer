2236c5db81fa842a7f7a4e5c847e945886a3e3ff
Matthew Dobie <mattdobie23@gmail.com>
2017-06-27 11:37:15 +1000

    Finish first working version

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+
+// Called when search or close icon is pressed
 function searchToggle(obj, evt) {
     var container = $(obj).closest(".search-box");
     var keyword = $(".search-input").val();
@@ -11,22 +13,22 @@ function searchToggle(obj, evt) {
       container.find(".search-input").val("");
     }
     else if (container.hasClass("active") && keyword !== "") {
-      console.log("searched!");
       search($(".search-input").val());
     }
   }
 
+// Search function called by searchToggle
 function search(input) {
     $(".header").hide();
     $(".random").hide();
     $("#results").empty();
-    $(".search-box").css("top", "-100px");
+    $(".search-box").animate({top: "-50px"}, 300);
 
     $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + encodeURI(input) + "&callback=?", function(data) {
       
-      console.log(data);
       var articleCount = 0;
-      
+      $("#results").append("<a href='' class='previous' style='opacity: 0'>&laquo;</a>");
+      $(".previous").animate({opacity: 1}, 300);
 
       if (data[1].length === 0) {
         $("#results").append("<div class='no-match'><h3>No Match</h3><p>There were no results. Try searching for something else!</p></div>");
@@ -43,63 +45,18 @@ function search(input) {
         $(".article-" + i).append("<h3>" + data[1][i] + "</h3>");
         $(".article-" + i).append("<p>" + data[2][i] + "</p>");
       }
-
     });
   }
 
 
 $(document).ready(function() {
-
-$(".search-box").keyup(function(e){
+  
+  // Search when space key is pressed
+  $(".search-box").keyup(function(e){
     var key = e.keyCode || e.which;
-    if(key === 13){
-      search($(".search-input").val());
+    if (key === 13) {
+      searchToggle(".search-icon", event);
     }
   });
 
 });
-
-/*
-$(document).ready(function(){
-  
-  
-  
-  $("input").keyup(function(e){
-    var key = e.keyCode || e.which;
-    if(key === 13){
-      $("#top-bar-input").autocomplete("close");
-      search($("#top-bar-input").val());
-    }
-  });
-  function search(input){
-    $("#wikipedia-results").empty();
-    $("#wikipedia-init").hide();
-    $(".container").removeClass("moz-container-fix");
-    $("#row-init").removeClass("moz-row-fix");
-    $("#wikipedia-top-bar").show();
-    $("#top-bar-input").focus();
-    if (input.length > 0){
-      $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + encodeURI(input) + "&callback=?", function(data){
-        if(data[1].length == 0){
-          $("#wikipedia-results").append("<div class='no-match'><p>There were no results matching the query.</p><p>The page <strong>\"" + $("#top-bar-input").val() + "\"</strong> does not exist. You can <a href='https://en.wikipedia.org/wiki/Wikipedia:Articles_for_creation' target='_blank' rel='noopener noreferrer'>ask for it to be created.</a></p><ul><li>Make sure that all words are spelled correctly.</li><li>Try different keywords.</li><li>Try more general keywords.</li></ul></div>");
-        }
-        for(var i = 0; i < data[1].length; i++){
-          $("#wikipedia-results").append("<div class='card card-" + i + "'></div>");
-          $(".card-"+i).append("<div class='card-content card-content-" + i + "'></div>");
-          $(".card-content-"+i).append("<h4>" + data[1][i] + "</h4>");
-          $(".card-content-"+i).append("<p>" + data[2][i] + "</p>");
-          if(data[2][i].length == 0){
-            $(".card-content-"+i).append("<p><i>No description available.</i></p>");
-          }
-          $(".card-"+i).append("<div class='card-link card-link-" + i + "'></div>");
-          $(".card-link-"+i).append("<a href='" + data[3][i] + "' target='_blank' rel='noopener noreferrer'>read more</a>");
-          $(".card-link-"+i).append("<a href='https://en.wikipedia.org/w/index.php?title=" + encodeURI(data[1][i]) + "&action=edit' target='_blank' rel='noopener noreferrer'>edit source</a>");
-        }
-      });
-      $("#wikipedia-results").show();
-      $("#top-bar-input").focus();
-      $("#top-bar-input")[0].setSelectionRange($("#top-bar-input").val().length, $("#top-bar-input").val().length);
-    }
-  }
-});
-*/
